762e88e2072187e43e3ffbb887e72d171e02238a
passion-logan <wangdadamailbox@163.com>
2020-03-03 12:40:06 +0800

    添加修改

--- src/main/java/com/cody/controller/HelloController.java
+++ src/main/java/com/cody/controller/HelloController.java
@@ -1,7 +1,14 @@
 package com.cody.controller;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * 应用模块名称
@@ -20,5 +27,25 @@ public class HelloController {
         return "hello";
     }
 
+    @PostMapping("uploadFile")
+    public @ResponseBody String uploadFile(@RequestParam(value = "file", required = true) MultipartFile file) {
+
+        try {
+
+            String fileName = file.getOriginalFilename();
+            String path = "E:\\download";
+            File files = new File(path + "\\" + fileName);
+            if (!files.getParentFile().exists()) {
+                files.getParentFile().mkdirs();
+            }
+
+            file.transferTo(files);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "上传失败";
+        }
+
+        return "上传成功";
+    }
 
 }
