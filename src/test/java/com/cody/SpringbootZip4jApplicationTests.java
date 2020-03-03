96b3760d5ca535f50ac70d9784a3cf85cf590e40
passion-logan <wangdadamailbox@163.com>
2020-03-03 10:34:43 +0800

    添加文件

---
+++ src/test/java/com/cody/SpringbootZip4jApplicationTests.java
@@ -0,0 +1,12 @@
+package com.cody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootZip4jApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
