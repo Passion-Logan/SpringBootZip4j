8a6fec8368194a1d3dcb6985ad3ee44910779ad2
passion-logan <wangdadamailbox@163.com>
2020-03-05 16:16:49 +0800

    添加ant解压方法

--- src/main/java/com/cody/controller/HelloController.java
+++ src/main/java/com/cody/controller/HelloController.java
@@ -48,7 +48,7 @@ public class HelloController {
      */
     @PostMapping("uploadFile")
     public @ResponseBody String uploadFile(@RequestParam(value = "file", required = true) MultipartFile file) {
-        return ZipFunction(file);
+        return AntFunntion(file);
     }
 
     /**
@@ -176,6 +176,72 @@ public class HelloController {
         } catch (IOException e) {
             e.printStackTrace();
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "上传成功";
+    }
+
+    @SuppressWarnings("unchecked")
+    public String AntFunntion(MultipartFile file) {
+        File files = null;
+
+        try {
+            String fileName = file.getOriginalFilename();
+            String path = "E:\\download";
+            files = new File(path + "\\" + fileName);
+            if (!files.getParentFile().exists()) {
+                files.getParentFile().mkdirs();
+            }
+
+            file.transferTo(files);
+
+            // 处理中文文件名乱码的问题
+            org.apache.tools.zip.ZipFile zipfile = new org.apache.tools.zip.ZipFile(path + "\\" + fileName, "GBK");
+
+            String strPath, gbkPath, strtemp;
+
+            UUID uuid = UUID.randomUUID();
+            String dest = "E:\\download\\" + uuid.toString();
+            File tempFile = new File(dest);
+
+            strPath = tempFile.getAbsolutePath();
+            Enumeration<?> e = zipfile.getEntries();
+            while (e.hasMoreElements()) {
+                org.apache.tools.zip.ZipEntry zipEnt = (org.apache.tools.zip.ZipEntry)e.nextElement();
+                gbkPath = zipEnt.getName();
+                if (zipEnt.isDirectory()) {
+                    strtemp = strPath + File.separator + gbkPath;
+                    File dir = new File(strtemp);
+                    dir.mkdirs();
+                    continue;
+                } else {
+                    // 读写文件
+                    InputStream is = zipfile.getInputStream(zipEnt);
+                    BufferedInputStream bis = new BufferedInputStream(is);
+                    gbkPath = zipEnt.getName();
+                    // 建目录
+                    strtemp = strPath + File.separator + gbkPath;
+                    String strsubdir = gbkPath;
+                    for (int i = 0; i < strsubdir.length(); i++) {
+                        if (strsubdir.substring(i, i + 1).equalsIgnoreCase("/")) {
+                            String temp = strPath + File.separator + strsubdir.substring(0, i);
+                            File subdir = new File(temp);
+                            if (!subdir.exists())
+                                subdir.mkdir();
+                        }
+                    }
+                    FileOutputStream fos = new FileOutputStream(strtemp);
+                    BufferedOutputStream bos = new BufferedOutputStream(fos);
+                    int c;
+                    while ((c = bis.read()) != -1) {
+                        bos.write((byte)c);
+                    }
+                    bos.close();
+                    fos.close();
+                }
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
