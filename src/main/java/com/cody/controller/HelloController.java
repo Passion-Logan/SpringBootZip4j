a9360db6e446e0c15696aaaad4395e08d707edb4
passion-logan <wangdadamailbox@163.com>
2020-03-05 16:05:39 +0800

    修改代码

--- src/main/java/com/cody/controller/HelloController.java
+++ src/main/java/com/cody/controller/HelloController.java
@@ -11,9 +11,6 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
 
-import net.lingala.zip4j.ZipFile;
-import net.lingala.zip4j.exception.ZipException;
-import net.lingala.zip4j.model.ExtraDataRecord;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +18,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import net.lingala.zip4j.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+import net.lingala.zip4j.model.ExtraDataRecord;
 import net.lingala.zip4j.model.FileHeader;
 
 /**
@@ -48,6 +48,16 @@ public class HelloController {
      */
     @PostMapping("uploadFile")
     public @ResponseBody String uploadFile(@RequestParam(value = "file", required = true) MultipartFile file) {
+        return ZipFunction(file);
+    }
+
+    /**
+     * zip4j 方法实现
+     *
+     * @param file
+     * @return
+     */
+    public String ZipFunction(MultipartFile file) {
         File files = null;
         try {
             String fileName = file.getOriginalFilename();
@@ -69,10 +79,10 @@ public class HelloController {
 
             String[] types = {"mp3", "mp4"};
             // 校验文件内容
-//            checkContent(zipFile, types);
+            // checkContent(zipFile, types);
 
             // 防止乱码
-            zipFile.setCharset(Charset.forName("utf-8"));
+            // zipFile.setCharset(Charset.forName("utf-8"));
 
             UUID uuid = UUID.randomUUID();
             String dest = "E:\\download\\" + uuid.toString();
@@ -102,6 +112,71 @@ public class HelloController {
             if (files != null) {
                 files.delete();
             }
+        }
+
+        return "上传成功";
+    }
+
+    /**
+     * 原生Java方法实现
+     *
+     * @param file
+     * @return
+     */
+    public String JavaFunction(MultipartFile file) {
+        File files = null;
+        java.util.zip.ZipFile zip = null;
+
+        try {
+            String fileName = file.getOriginalFilename();
+
+            String path = "E:\\download";
+            files = new File(path + "\\" + fileName);
+            if (!files.getParentFile().exists()) {
+                files.getParentFile().mkdirs();
+            }
+            file.transferTo(files);
+
+            // 指定解压后的文件夹
+            UUID uuid = UUID.randomUUID();
+            String dest = "E:\\download\\" + uuid.toString();
+            File destDir = new File(dest);
+            destDir.mkdir();
+
+            String encoding = getEncoding(path + "\\" + fileName);
+            zip = new java.util.zip.ZipFile(files, Charset.forName(encoding));
+            for (Enumeration entries = zip.entries(); entries.hasMoreElements();) {
+                ZipEntry entry = (ZipEntry)entries.nextElement();
+                String zipEntryName = entry.getName();
+                InputStream in = zip.getInputStream(entry);
+
+                // 指定文件路径+当前zip文件的名称
+                String outPath = (destDir + "/" + zipEntryName).replace("/", File.separator);
+
+                if (!entry.isDirectory()) {
+                    // 保存文件路径信息（可利用md5.zip名称的唯一性，来判断是否已经解压）
+                    OutputStream out = new FileOutputStream(outPath);
+                    byte[] buf1 = new byte[2048];
+                    int len;
+                    while ((len = in.read(buf1)) > 0) {
+                        out.write(buf1, 0, len);
+                    }
+                    in.close();
+                    out.close();
+                } else {
+                    // 判断路径是否存在,不存在则创建文件路径
+                    File filePath = new File(outPath.substring(0, outPath.lastIndexOf(File.separator)));
+                    if (!filePath.exists()) {
+                        filePath.mkdirs();
+                    }
+                }
+            }
+            // 必须关闭，要不然这个zip文件一直被占用着，要删删不掉，改名也不可以，移动也不行，整多了，系统还崩了。
+            zip.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
         return "上传成功";
@@ -154,73 +229,10 @@ public class HelloController {
         return fileHeader.getFileName();
     }
 
-    /**
-     * // * 文件类型： MP3 MP4 pdf png 文件夹 // * // * @param file // * @return //
-     */
-    // @PostMapping("uploadFile")
-    // public @ResponseBody String uploadFile(@RequestParam(value = "file", required = true) MultipartFile file) {
-    // File files = null;
-    // ZipFile zip = null;
-    //
-    // try {
-    // String fileName = file.getOriginalFilename();
-    //
-    // String path = "E:\\download";
-    // files = new File(path + "\\" + fileName);
-    // if (!files.getParentFile().exists()) {
-    // files.getParentFile().mkdirs();
-    // }
-    // file.transferTo(files);
-    //
-    // //指定解压后的文件夹
-    // UUID uuid=UUID.randomUUID();
-    // String dest = "E:\\download\\" + uuid.toString();
-    // File destDir = new File(dest);
-    // destDir.mkdir();
-    //
-    // String encoding = getEncoding(path + "\\" + fileName);
-    // zip = new ZipFile(files, Charset.forName(encoding));
-    // for (Enumeration entries = zip.entries(); entries.hasMoreElements();) {
-    // ZipEntry entry = (ZipEntry) entries.nextElement();
-    // String zipEntryName = entry.getName();
-    // InputStream in = zip.getInputStream(entry);
-    //
-    // //指定文件路径+当前zip文件的名称
-    // String outPath = (destDir + "/" + zipEntryName).replace("/", File.separator);
-    //
-    // if (!entry.isDirectory()) {
-    // //保存文件路径信息（可利用md5.zip名称的唯一性，来判断是否已经解压）
-    // OutputStream out = new FileOutputStream(outPath);
-    // byte[] buf1 = new byte[2048];
-    // int len;
-    // while ((len = in.read(buf1)) > 0) {
-    // out.write(buf1, 0, len);
-    // }
-    // in.close();
-    // out.close();
-    // } else {
-    // //判断路径是否存在,不存在则创建文件路径
-    // File filePath = new File(outPath.substring(0, outPath.lastIndexOf(File.separator)));
-    // if (!filePath.exists()) {
-    // filePath.mkdirs();
-    // }
-    // }
-    // }
-    // //必须关闭，要不然这个zip文件一直被占用着，要删删不掉，改名也不可以，移动也不行，整多了，系统还崩了。
-    // zip.close();
-    // } catch (IOException e) {
-    // e.printStackTrace();
-    // } catch (Exception e) {
-    // e.printStackTrace();
-    // }
-    //
-    // return "上传成功";
-    // }
-
     @SuppressWarnings("unchecked")
     private static String getEncoding(String path) throws Exception {
         String encoding = "GBK";
-        net.lingala.zip4j.ZipFile zipFile = new net.lingala.zip4j.ZipFile(path);
+        ZipFile zipFile = new ZipFile(path);
         zipFile.setCharset(Charset.forName(encoding));
         List<FileHeader> list = zipFile.getFileHeaders();
         for (int i = 0; i < list.size(); i++) {
@@ -233,23 +245,6 @@ public class HelloController {
         }
         return encoding;
     }
-
-    // @SuppressWarnings("unchecked")
-    // private static String getEncoding(String path) throws Exception {
-    // String encoding = "GBK";
-    // net.lingala.zip4j.ZipFile zipFile = new net.lingala.zip4j.ZipFile(path);
-    // zipFile.setCharset(Charset.forName(encoding));
-    // List<FileHeader> list = zipFile.getFileHeaders();
-    // for (int i = 0; i < list.size(); i++) {
-    // FileHeader fileHeader = list.get(i);
-    // String fileName = fileHeader.getFileName();
-    // if (isMessyCode(fileName)) {
-    // encoding = "UTF-8";
-    // break;
-    // }
-    // }
-    // return encoding;
-    // }
 
     private static boolean isMessyCode(String str) {
         for (int i = 0; i < str.length(); i++) {
