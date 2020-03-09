57d3e4aff4179d6f076aafcdff75e9d062d9a080
Passion-Logan <wangdadamailbox@163.com>
2020-03-09 23:29:00 +0800

    添加修改

--- src/main/java/com/cody/controller/testController.java
+++ src/main/java/com/cody/controller/testController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * ClassName: testController
  *
@@ -18,7 +21,8 @@ public class testController {
 
 
     @GetMapping("hello")
-    public String test() {
-        return "测试连接";
+    public void test(HttpServletResponse response) throws IOException {
+        response.setStatus(200);
+        response.getWriter().append("success");
     }
 }
