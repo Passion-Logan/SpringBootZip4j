aaa90d752ca2b10854574e6b0b115f0a7ea03345
Passion-Logan <wangdadamailbox@163.com>
2020-03-13 22:56:13 +0800

    添加修改

--- src/main/java/com/cody/controller/newUploaderController.java
+++ src/main/java/com/cody/controller/newUploaderController.java
@@ -1,5 +1,9 @@
 package com.cody.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * ClassName: newUploaderController
  *
@@ -9,4 +13,15 @@ package com.cody.controller;
  * @since JDK 1.8
  */
 public class newUploaderController {
+
+    /**
+     * @Description: 减产分片是否上传
+     * @Date: 2020/3/13
+     * @Param response:
+     * @return: void
+     */
+    @GetMapping("chunk")
+    public void chunk(HttpServletResponse response) {
+
+    }
 }
