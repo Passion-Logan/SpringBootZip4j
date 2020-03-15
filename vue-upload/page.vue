869ee4c68f22045755cf99c68b5e534fb2ce8c21
Passion-Logan <wangdadamailbox@163.com>
2020-03-15 23:08:54 +0800

    添加使用方法

---
+++ vue-upload/page.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="page">
+    <!-- <div id="filePicker">选择文件</div>
+    <NewWebUpload
+      :form-data="urlToken"
+      :url="serverUrl"
+      :accept-types="acceptTypes"
+      upload-button="#filePicker"
+      multiple/> -->
+
+    <globalUploaders/>
+  </div>
+</template>
+
+<script>
+
+// import { getToken } from '@/utils/auth'
+// import $ from 'jquery'
+
+// export default {
+//   components: {
+//   },
+//   data () {
+//     return {
+//       serverUrl: process.env.BASE_API + '/new/upload/uploadFile',
+//       urlToken: {},
+//       acceptTypes: {
+//         title: 'Files',
+//         extensions: 'jpg,jpeg,png,pdf,txt,zip,mp3,mp4',
+//         mimeTypes: 'image/png, application/pdf, audio/mpeg, audio/mp4, video/mp4, image/jpeg, text/plain, application/zip'
+//       }
+//     }
+//   },
+//   computed: {
+//   },
+//   watch: {},
+//   created () {
+//     this.urlToken.token = getToken()
+//   },
+//   mounted () {
+//   },
+//   methods: {
+
+//   }
+// }
+</script>
+
