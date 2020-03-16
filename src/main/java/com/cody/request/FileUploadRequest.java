e2795e648524ce2b9c2185e64634b9286f56552e
passion-logan <wangdadamailbox@163.com>
2020-03-16 19:01:33 +0800

    添加request

---
+++ src/main/java/com/cody/request/FileUploadRequest.java
@@ -0,0 +1,12 @@
+package com.cody.request;
+
+/**
+ * ClassName: FileUploadRequest
+ *
+ * @author WQL
+ * @Description:
+ * @date: 2020年3月16日 0016 19:01
+ * @since JDK 1.8
+ */
+public class FileUploadRequest {
+}
