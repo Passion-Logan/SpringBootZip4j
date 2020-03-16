ae37cd305d4031123e7246710f268b1517baedc8
passion-logan <wangdadamailbox@163.com>
2020-03-16 19:05:56 +0800

    修改response

--- src/main/java/com/cody/response/FileUploadResponse.java
+++ src/main/java/com/cody/response/FileUploadResponse.java
@@ -1,5 +1,7 @@
 package com.cody.response;
 
+import java.util.ArrayList;
+
 import lombok.Data;
 
 /**
@@ -12,4 +14,25 @@ import lombok.Data;
  */
 @Data
 public class FileUploadResponse {
+
+    /**
+     * 是否合并
+     */
+    boolean needMerge;
+
+    /**
+     * 存放的路径
+     */
+    String path;
+
+    /**
+     * 文件名称
+     */
+    String name;
+
+    /**
+     * 已经上传的分块
+     */
+    ArrayList uploadde;
+
 }
