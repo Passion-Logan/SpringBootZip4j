318a2bf030517e678a17fa4b1bc96852e40502c7
passion-logan <wangdadamailbox@163.com>
2020-03-16 19:04:43 +0800

    修改request

--- src/main/java/com/cody/request/FileUploadRequest.java
+++ src/main/java/com/cody/request/FileUploadRequest.java
@@ -1,6 +1,7 @@
 package com.cody.request;
 
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * ClassName: FileUploadRequest
@@ -12,4 +13,40 @@ import lombok.Data;
  */
 @Data
 public class FileUploadRequest {
+
+    /**
+     * 当前文件块，从1开始
+     */
+    private Integer chunkNumber;
+    /**
+     * 分块大小
+     */
+    private Long chunkSize;
+    /**
+     * 当前分块大小
+     */
+    private Long currentChunkSize;
+    /**
+     * 总大小
+     */
+    private Long totalSize;
+    /**
+     * 文件标识
+     */
+    private String identifier;
+    /**
+     * 文件名
+     */
+    private String filename;
+    /**
+     * 相对路径
+     */
+    private String relativePath;
+    /**
+     * 总块数
+     */
+    private Integer totalChunks;
+
+    private MultipartFile file;
+
 }
