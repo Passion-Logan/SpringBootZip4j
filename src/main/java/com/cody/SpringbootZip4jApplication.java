0b17adb50cc3faefbc1d362448b114e9b9c1be1e
Passion-Logan <wangdadamailbox@163.com>
2020-03-24 22:29:24 +0800

    添加注释

--- src/main/java/com/cody/SpringbootZip4jApplication.java
+++ src/main/java/com/cody/SpringbootZip4jApplication.java
@@ -3,6 +3,14 @@ package com.cody;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * idea静态资源访问路径
+ * 1 classpath:/META-INF/resources/
+ * 2 classpath:/resources/
+ * 3 classpath:/static/
+ * 4 classpath:/public/
+ * 5 /:当前项目的根路径
+ */
 @SpringBootApplication
 public class SpringbootZip4jApplication {
 
